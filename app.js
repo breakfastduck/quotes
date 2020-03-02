a7c921c6294e3be1260bf08816859af34b971f54
breakfastduck <52973745+breakfastduck@users.noreply.github.com>
2020-03-02 22:02:26 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+// var http = require('http');
+// var server = http.createServer(function(req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     var message = 'It works!\n',
+//         version = 'NodeJS ' + process.versions.node + '\n',
+//         response = [message, version].join('\n');
+//     res.end(response);
+// });
+// server.listen();
+
+
+
+const express = require('express')
+require('./src/mongoose')
+const Quote = require('./src/quoteModel')
+
+const app = express()
+const port = process.env.PORT || 3000
+//const port = ''
+app.use(express.json())
+
+app.get('/help', (req, res) => res.send("Hello World!"));
+
+
+app.post('/quotes', async (req, res) => {
+    const quote = new Quote(req.body)
+
+    try {
+        await quote.save()
+        res.status(201).send(quote)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+app.get('/quote', async (req, res) => {
+    
+    try {
+        const quote = await Quote.findOne({ quoteID })
+
+        if (!quote) {
+            throw new Error('Unable to login')
+        }
+        
+        res.send(quote)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+    
+
+    
+})
+
+
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
